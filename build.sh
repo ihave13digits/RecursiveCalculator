c89651037876cf71f1a4e8c182f4d433b20f6766
digits <44655871+ihave13digits@users.noreply.github.com>
2024-05-20 11:15:42 -0400

    Bug fixes, error checking

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 clear
 if g++ -o solve main.cpp -lstdc++fs -fopenmp -std=c++17; then
-    ./solve "(((((1-2)+3)/((4)*(5^6)))))"
+    ./solve "((1-2) + 3) / ((4) * (5^6))"
+    ./solve
 else
     echo "G++ Build Failed"
 fi
--- main.cpp
+++ main.cpp
@@ -6,16 +6,7 @@
 
 
 
-template <typename T> void Print(T data, std::string end="\n", int padding=0)
-{
-    if (padding==0) { if (end=="\n") { std::cout<<data<<std::endl; } else  { std::cout<<data<<end; } }
-    else
-    {
-        std::string pad_string = "";
-        for (int i=0; i<padding; i++) { pad_string=pad_string+" "; }
-        if (end=="\n") { std::cout<<data<<std::endl; } else  { std::cout<<data<<pad_string<<end; }
-    }
-}
+template <typename T> void Print(T data, std::string end="\n") { if (end=="\n") { std::cout<<data<<std::endl; } else { std::cout<<data<<end; } }
 
 
 
@@ -32,50 +23,20 @@ private:
 public:
 
     bool IsNumber(std::string data)
-    {
-        for (int i=0; i<data.size(); i++)
-        {
-            char c = data.at(i);
-            if (c<'0' || c>'9') { return false; }
-        }
-        return true;
-    }
-
-    bool IsOperation(std::string data)
-    {
-        if (data.size()==1)
-        {
-            char c = data.at(0);
-            if ((c>41 && c<46) || c=='^' || c=='/') { return true; }
-        }
-        return false;
-    }
-
-    bool IsValidCharacter(char c)
-    {
-        if ((c>39 && c<58) || c=='^' || c=='/') { return true; }
-        return false;
-    }
-
-    void PrintTokens(std::vector<std::string> tokens)
-    {
-        Print(symbols[state], " ");
-        for (int i=0; i<tokens.size(); i++)
-        {
-            Print(tokens[i], "");
-        }
-        Print("");//Print("\n");
-    }
+    { int dc = 0; for (int i=0; i<data.size(); i++) { char c = data.at(i); if (c=='.') { dc++; } if ((c<'0' || c>'9') && c!='.') { return false; } } if (dc>1) { return false; } return true; }
+    bool IsOperation(std::string data) { if (data.size()==1) { char c = data.at(0); if ((c>41 && c<46) || c=='^' || c=='/') { return true; } } return false; }
+    bool IsValidCharacter(char c) { if ((c>39 && c<58) || c=='^' || c=='/' || c=='.') { return true; } return false; }
 
     void ToggleShowWork() { show_work=!show_work; }
-    std::vector<std::string> Error(std::string text) { return {"Parentheses Error: "+text}; }
+    std::vector<std::string> Error(std::string text, std::string type) { return {type+" Error: "+text}; }
+    void PrintTokens(std::vector<std::string> tokens) { Print(symbols[state], " "); for (int i=0; i<tokens.size(); i++) { Print(tokens[i], ""); } Print(""); }
 
 
 
     std::string SolveProblem(std::string op, std::string A, std::string B)
     {
         if (show_work) { Print(A+" "+op+" "+B); }
-        float a=std::stof(A), b=std::stof(B);
+        double a=std::stod(A), b=std::stod(B);
         float c=0.0;
         if      (op=="^") { c=pow(a,b);}
         else if (op=="*") { c=    a*b; }
@@ -133,7 +94,7 @@ public:
                 else if (c=='/') { can_push = true; glyph = "/"; }
                 else if (c=='+') { can_push = true; glyph = "+"; }
                 else if (c=='-') { can_push = true; glyph = "-"; }
-                else { token+=c; }
+                else { if (c!=' ') { token+=c; } }
                 if (can_push)
                 {
                     if (token.size()>0)
@@ -170,24 +131,13 @@ public:
                     _t=tokens[i-1];
                     if (token=="-")
                     {
-                        if (_t=="(" || _t==")" || _t=="^" || _t=="*" || _t=="/" || _t=="+" || _t=="-")
-                        {
-                            found_negative = true;
-                            std::string n = tokens[i]+tokens[i+1];
-                            tokens.erase(tokens.begin()+i);
-                            tokens[i] = n;
-                        }
+                        if (_t=="(" || _t==")" || _t=="^" || _t=="*" || _t=="/" || _t=="+" || _t=="-" || _t==".")
+                        { found_negative = true; std::string n = tokens[i]+tokens[i+1]; tokens.erase(tokens.begin()+i); tokens[i] = n; }
                     }
                 }
                 else
                 {
-                    if (token=="-")
-                    {
-                        found_negative = true;
-                        std::string n = tokens[i]+tokens[i+1];
-                        tokens.erase(tokens.begin()+i);
-                        tokens[i] = n;
-                    }
+                    if (token=="-") { found_negative = true; std::string n = tokens[i]+tokens[i+1]; tokens.erase(tokens.begin()+i); tokens[i] = n; }
                 }
             }
             if (!found_negative) { break; }
@@ -195,39 +145,41 @@ public:
         return tokens;
     }
 
+    std::vector<std::string> ParseErrors(std::vector<std::string> tokens)
+    {
+        int L=0, R=0, N=0, O=0;
+        for (int i=0; i<tokens.size(); i++)
+        {
+            if      (tokens[i]=="(") { L++; }
+            else if (tokens[i]==")") { R++; }
+            else if (tokens[i]=="^") { O++; }
+            else if (tokens[i]=="*") { O++; }
+            else if (tokens[i]=="/") { O++; }
+            else if (tokens[i]=="+") { O++; }
+            else if (tokens[i]=="-") { O++; }
+            else if (IsNumber(tokens[i])) { N++; }
+        }
+        if (L!=R) { return Error("Mismatched Parentheses", "Parentheses"); }
+        if (O>=N) { return Error("Invalid Expression", "Expression"); }
+        return tokens;
+    }
+
     std::vector<std::string> ParseParentheses(std::vector<std::string> tokens)
     {
-        int L=0, R=0;
-        for (int i=0; i<tokens.size(); i++) { if (tokens[i]=="(") { L++; } else if (tokens[i]==")") { R++; } }
-        if (L!=R) { return Error("Mismatched Parentheses"); }
+        tokens = ParseErrors(tokens);
         int _count = 2;
         while(_count>0)
         {
             state=6; if (show_work) { PrintTokens(tokens); } state=1;
-            bool found_end = false;
-            int li=-1, ri=-1, lc=0, rc=0;
+            bool found_end = false; int li=-1, ri=-1, lc=0, rc=0;
             for (int i=0; i<tokens.size(); i++)
             {
-                if      (tokens[i]=="(")
-                {
-                    lc++;
-                    if (i>li && !found_end) { li=i; }
-                }
-                else if (tokens[i]==")")
-                {
-                    if (!found_end) { ri=i; }
-                    found_end=true;
-                }
+                if      (tokens[i]=="(") { lc++; if (i>li && !found_end) { li=i; } }
+                else if (tokens[i]==")") { if (!found_end) { ri=i; } found_end=true; }
             }
             if (li<0 || ri<0) { break; }
-            std::vector<std::string> expression;
-            int diff = ri-li;
-            for (int i=0; i<diff+1; i++)
-            {
-                std::string t = tokens[li];
-                if (t!="(" && t!=")") { expression.push_back(t); }
-                tokens.erase(tokens.begin()+li);
-            }
+            std::vector<std::string> expression; int diff = ri-li;
+            for (int i=0; i<diff+1; i++) { std::string t = tokens[li]; if (t!="(" && t!=")") { expression.push_back(t); } tokens.erase(tokens.begin()+li); }
             while (expression.size()>1) { expression=SolveExpression(expression); }
             if (expression.size()>0) { tokens.insert(tokens.begin()+li, expression[0]); }//lc--; rc--; }
             _count=lc+rc;
@@ -237,16 +189,11 @@ public:
 
     std::string Parse(std::string data)
     {
-        std::vector<std::string> tokens = Tokenize(data);
-        std::vector<std::string> problem = ParseParentheses(tokens);
+        std::vector<std::string> tokens = Tokenize(data), problem = ParseParentheses(tokens);
         if (problem.size()==1) { return problem[0]; }
         else { state=1; tokens = problem; }
         std::string result = "";
-        while(tokens.size()>1)
-        {
-            if (show_work) { PrintTokens(tokens); }
-            tokens = SolveExpression(tokens);
-        }
+        while(tokens.size()>1) { if (show_work) { PrintTokens(tokens); } tokens = SolveExpression(tokens); }
         result = tokens[0];
         return result;
     }
@@ -259,23 +206,14 @@ int main(int argc, char *argv[])
 {
     Parser parser = Parser();
     std::string expression = "";
-    if (argc>1)
-    {
-        
-        for (int i=1; i<argc; i++) { expression += argv[i]; }
-        Print(parser.Parse(expression));
-    }
+    if (argc>1) { for (int i=1; i<argc; i++) { expression += argv[i]; } Print(parser.Parse(expression)); }
     else
     {
-        bool user_engaged = true;
-        while (user_engaged)
+        bool user_engaged = true; while (user_engaged)
         {
             Print("Enter an expression to solve, 'x' to exit, or 's' to toggle showing work");
-            for (int i=0; i<4; i++) { Print(""); }
-            expression = ""; std::cin >> expression;
-            if      (expression=="x") { user_engaged=false; return 0; }
-            else if (expression=="s") { parser.ToggleShowWork(); }
-            else                      { Print(parser.Parse(expression)); }
+            for (int i=0; i<4; i++) { Print(""); } expression = ""; std::getline(std::cin, expression);
+            if (expression=="x") { user_engaged=false; return 0; } else if (expression=="s") { parser.ToggleShowWork(); } else { Print(parser.Parse(expression)); }
         }
     }
     return 0;
